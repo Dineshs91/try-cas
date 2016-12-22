2c6248320f2f5f5ee84cd14d90d3d58573fdceaa
dinesh <dineshpy07@gmail.com>
2016-12-23 01:33:49 +0530

    Add support to store content from a file

--- client.py
+++ client.py
@@ -5,18 +5,27 @@ from api.api import API
 
 parser = argparse.ArgumentParser(description="content addressable storage")
 parser.add_argument('action', nargs=1, help='cas store <content>')
-parser.add_argument('content', nargs=1, help='cas fetch <sha>')
+parser.add_argument('content', nargs='?', help='cas fetch <sha>')
+parser.add_argument('-f', help='store content of a file')
 
 args = parser.parse_args()
 
 action = args.action[0]
-content = args.content[0]
+
+content = args.content
 
 api = API()
 
-if action == 'store':
+if action == 'store' and args.f:
+    file = args.f
+    with open(file, 'r+') as f:
+        content = f.read()
+    sha = api.store(content.encode('utf-8'))
+    print(sha)
+elif action == 'store':
     sha = api.store(content.encode('utf-8'))
     print(sha)
 elif action == 'fetch':
+    print(content)
     data = api.fetch(content)
     print(data.decode('utf-8'))
