80addc579c1afb26984660f63c4bdcca800c40fe
dinesh <dineshpy07@gmail.com>
2016-12-23 01:24:53 +0530

    Add support to fetch data with a minimum of 5 sha chars

--- core/filesystem.py
+++ core/filesystem.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 
 class FileSystem:
@@ -18,9 +19,36 @@ class FileSystem:
 
     def get_file_and_dir_name(self, sha):
         dirname = sha[:2]
-        filename = sha[2:40]
+        filename = sha[2:]
 
         return dirname, filename
+
+    def get_file_and_dir_name_smart(self, sha):
+        dirname = sha[:2]
+        filename = sha[2:]
+
+        # Go through all files to see how many matches can be found
+        # for filename. If only one is found, return.
+        # Else raise exception.
+        complete_filename = self.search_file(dirname, filename)
+        return dirname, complete_filename
+
+    def search_file(self, dirname, filename):
+        count = 0
+
+        files = os.listdir(os.path.join(self.cas_dir, 'objects', dirname))
+        complete_filename = ''
+
+        for f in files:
+            if f[0:len(filename)] == filename:
+                complete_filename = f
+                count += 1
+
+        if count > 1:
+            print("Multiple files found")
+        elif count == 0:
+            print("Unable to find the content")
+        return complete_filename
 
     def get_file_path(self, dirname, filename):
         file_path = os.path.join(self.cas_dir, 'objects', dirname,
@@ -40,7 +68,17 @@ class FileSystem:
             f.write(content)
 
     def read(self, sha):
-        dirname, filename = self.get_file_and_dir_name(sha)
+        """
+        Support fetch using just 5 chars from sha
+        """
+        sha_length = len(sha)
+        if sha_length < 5:
+            print('Not enough sha chars to fetch content')
+            sys.exit(1)
+        if sha_length != 40:
+            dirname, filename = self.get_file_and_dir_name_smart(sha)
+        else:
+            dirname, filename = self.get_file_and_dir_name(sha)
         file_path = self.get_file_path(dirname, filename)
 
         if not os.path.exists(file_path):
